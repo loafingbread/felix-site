38feb5e24cfb456a95a49428da7f49ceefc66119
loafingbread <felixle96@gmail.com>
2025-08-06 00:35:24 -0700

    Add navigation and about me page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './routes/Home.tsx'
+import App from './App.tsx'
+import Navbar from './components/Navbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <App />
   </StrictMode>,
 )
---
+++ src/routes/AboutMe.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function AboutMe() {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <article>
+                <section>
+                    <h2>Who am I?</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Pellentesque euismod, nisi eu consectetur cursus, enim erat
+                        dictum urna, nec dictum velit enim at erat. Mauris ac facilisis
+                        erat, nec laoreet enim.
+                    </p>
+                    <p>
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque laudantium. Quisque euismod, urna eu
+                        tincidunt consectetur, nisi nisl aliquam enim, vitae facilisis
+                        enim erat nec enim.
+                    </p>
+                    <p>
+                        Curabitur non nulla sit amet nisl tempus convallis quis ac
+                        lectus. Proin eget tortor risus. Vestibulum ac diam sit amet
+                        quam vehicula elementum sed sit amet dui.
+                    </p>
+                </section>
+            </article>
+        </main>
+    )
+}
+
+export default AboutMe;
