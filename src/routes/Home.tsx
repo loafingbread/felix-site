c01e2b5eff8f5d7b2d999e626343690a75c2eb76
loafingbread <felixle96@gmail.com>
2025-08-06 00:17:48 -0700

    Add unstyled introduction to home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import profile from "../assets/profile.jpg";
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+        <main>
+            <header>
+                <h1>Home</h1>
+            </header>
+            <article>
+                <section>
+                    <img src={profile} alt="Felix Le" />
+                </section>
+                <section>
+                    <h2>Hi! I'm Felix Le.</h2>
+                    <p>
+                        I'm a software engineer with a focus on web development and a
+                        passion for playing and making video games.
+                    </p>
+                </section>
+            </article>
+        </main>
+    );
 }
 
 export default Home;
